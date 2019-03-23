362926752625741693d7409342e5b8b1ab7e6613
Miranda J. Ignacio <mirandajignacio@gmail.com>
2019-03-23 09:49:30 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
-import Link from 'next/link'
+import Link from "next/link";
 
 export default function Index() {
   return (
     <div>
-      <h1>Hello Next.js 👋</h1>
-      <Link href='/about'><a>About</a></Link>
+      <h1>Hello Next.js 👋 Loquita</h1>
+      <Link href="/about">
+        <a>About</a>
+      </Link>
     </div>
-  )
+  );
 }
